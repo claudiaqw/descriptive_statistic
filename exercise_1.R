86670995ade6c5f4be7f9f38ca777da3af5b0654
Claudia Quintana Wong <cquintana@alumni.afi.es>
2021-01-21 20:28:25 +0100

    linear model deployed

--- exercise_1.R
+++ exercise_1.R
@@ -64,7 +64,7 @@ apply(X = Wage[, numeric_cols], MARGIN = 2, FUN = mean, na.rm = T)
 apply(X = Wage[, numeric_cols], MARGIN = 2, FUN = median, na.rm = T)
 
 #Cuantiles
-apply(X = Wage[, numeric_cols], MARGIN = 2, FUN = quantile(), na.rm = T)
+apply(X = Wage[, numeric_cols], MARGIN = 2, FUN = quantile, na.rm = T)
 
 
 #Estadísticos de dispersión
@@ -86,6 +86,28 @@ apply(X = Wage[, numeric_cols], MARGIN = 2, FUN = skewness, na.rm = T)
 apply(X = Wage[, numeric_cols], MARGIN = 2, FUN = kurtosis, na.rm = T)
 
 
+#Ajusta un modelo de regresión lineal para predecir logwage usando 
+#age, jobclass, race y health_ins
+library(dplyr)
+
+unique(Wage$region) #como existe una sola región lo quitamos porque no nos va a dar mucha información
+
+pairs(Wage[-6], col = 'red')
+
+lm_logwage <- lm(logwage ~ age + jobclass + race + health_ins, data=Wage)
+lm_logwage
+
+#model
+#logwage = 0.0048*age + 0.1045*jobclass2.Information - 0.1056*race2. Black + 0.0624*race3. Asian - -0.1419*race4. Other - 0.2461*health_ins2. No + 4.4798
+
+summary(lm_logwage)
+
+confint(lm_logwage, level = 0.95) #intervalos de confianza
+
+#plot(Wage$logwage, Wage$Sales, type = 'p', col = 'red', xlab = 'TV', ylab = 'Sales')
+#abline(lm_logwage, col = 'blue')
+
+plot(Wage$logwage, lm_logwage$residuals, type = 'p', col = 'red', xlab = 'TV', ylab = 'Sales')
 
 
   
